52540c122ffa3728250b7db33d7cf5fdbb8035ba
JoaoPedroVisconti <visconti.joao@gmail.com>
2022-04-22 15:24:31 +0100

    Improve the function for scale and run the model

--- django_api/api/functions/PrepData.py
+++ django_api/api/functions/PrepData.py
@@ -3,19 +3,39 @@ from keras.models import load_model
 import pandas as pd
 import joblib
 
-# Load scaler
-scaler, dummies_frame = joblib.load(
-    'F:\\Visconti\\DevProgram\\Tutorial\\Django\\MachineLearning\\DjangoAPI_MachineLearning\\machine_learning\\model\\scaler.jbl')
+import os
+
+path_parent = os.path.dirname(os.getcwd())
+path_model = (path_parent + '\\machine_learning\\model\\loan_model')
+path_scaler = (path_parent + '\\machine_learning\\model\\scaler.jbl')
 
 
-print('Running Scaler ...')
+# Load model and scaler
+model = load_model(path_model)
+scaler, dummies_frame = joblib.load(path_scaler)
 
 
 def scale_data(data):
-  df = pd.DataFrame(data)
+  # Grab the input data from the request, convert to DataFrame
+  df = pd.DataFrame(data, index=[0])
+
   df = pd.get_dummies(df)
   df = df.reindex(columns=dummies_frame, fill_value=0)
 
   unit = scaler.transform(df)
 
   return unit
+
+
+def submit_candidature(data):
+
+  unit = scale_data(data)
+
+  # Run model to predict
+  y_pred = model.predict(unit)
+  y_pred = (y_pred > 0.55)
+
+  new_df = pd.DataFrame(y_pred, columns=['Status'])
+  new_df = new_df.replace({True: 'Approved', False: 'Rejected'})
+
+  return 'Your Status is {}'.format(new_df.values)
--- django_api/api/views.py
+++ django_api/api/views.py
@@ -11,7 +11,7 @@ import numpy as np
 import pandas as pd
 from keras.models import load_model
 
-from .functions.PrepData import scale_data
+from .functions import PrepData
 
 # Create your views here.
 
@@ -29,21 +29,10 @@ def submitCandidature(request):
 
   try:
 
-    mdl = load_model(
-        'F:\\Visconti\\DevProgram\\Tutorial\\Django\\MachineLearning\\DjangoAPI_MachineLearning\\machine_learning\\model\\loan_model')
+    data = request.data
+    prediction = PrepData.submit_candidature(data)
 
-    # Grab the input data from the request, convert to DataFrame
-    df_data = pd.DataFrame(request.data, index=[0])
-    unit = scale_data(df_data)
-
-    # Run model to predict
-    y_pred = mdl.predict(unit)
-    y_pred = (y_pred > 0.55)
-
-    new_df = pd.DataFrame(y_pred, columns=['Status'])
-    new_df = new_df.replace({True: 'Approved', False: 'Rejected'})
-
-    return JsonResponse('Your Status is {}'.format(new_df), safe=False)
+    return JsonResponse(prediction, safe=False)
 
   except ValueError as e:
     return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
